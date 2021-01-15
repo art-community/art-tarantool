d2729b640ac0841f53df4b4bdd285823738cbd77
antomy-gc <antomy.gc@gmail.com>
2021-01-16 01:31:25 +0300

    init

---
+++ art/storage/init.lua
@@ -0,0 +1,15 @@
+art = {
+    core = require('art.core'),
+
+    config = require('art.storage.config'),
+
+    box = require('art.storage.box'),
+
+    cluster = require('art.storage.cluster'),
+
+    transaction = require('art.storage.transaction'),
+
+    api = require('art.storage.api')
+}
+
+return art
