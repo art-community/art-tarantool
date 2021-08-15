228841818d40b4cb6307fdee10a7f5b27996f324
anton.bashirov <anton.sh.local@gmail.com>
2021-08-15 09:28:31 +0300

    ART

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,6 +19,7 @@
 rootProject.name = "art-tarantool"
 
 pluginManagement {
+    val internalPluginVersion: String by settings
     repositories {
         gradlePluginPortal()
         maven { url = uri("https://nexus.art-platform.io/repository/art-gradle-plugins/") }
@@ -26,7 +27,7 @@ pluginManagement {
     resolutionStrategy {
         eachPlugin {
             if (requested.id.id.contains("art")) {
-                useModule("io.art.gradle:art-gradle:${requested.version}}")
+                useModule("io.art.gradle:art-gradle:$internalPluginVersion}")
             }
         }
     }
