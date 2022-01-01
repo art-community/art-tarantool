ed960cba670f35f4e20873f8be8118f5c17fe2ca
anton.bashirov <anton.sh.local@gmail.com>
2022-01-02 00:45:10 +0300

    ART

--- src/main/lua/art/core/init.lua
+++ src/main/lua/art/core/init.lua
@@ -1,4 +1,8 @@
 local core = {
+    test = function()
+        require("log").info("test")
+    end,
+
     schemaOf = function(space)
         return box.space['_' .. space .. art.config.schemaPostfix]
     end,
@@ -29,43 +33,57 @@ local core = {
     end,
 
     atomic = function(func, ...)
-        if box.is_in_txn() then return func(...) end
+        if box.is_in_txn() then
+            return func(...)
+        end
         return box.atomic(func, ...)
     end,
 
     functional = require('fun'),
 
     bucketFieldNumber = function(space)
-        if not(box.space[space]) or not(box.space[space].index.bucket_id) then return end
+        if not (box.space[space]) or not (box.space[space].index.bucket_id) then
+            return
+        end
         return box.space[space].index.bucket_id.parts[1].fieldno
     end,
 
     bucketFromData = function(space, data)
-        if not(box.space[space].index.bucket_id) then return end
-        return data[1][ art.core.bucketFieldNumber(space) ]
+        if not (box.space[space].index.bucket_id) then
+            return
+        end
+        return data[1][art.core.bucketFieldNumber(space)]
     end,
 
     insertBucket = function(space, data, bucket_id)
-        if not(data) or not(bucket_id) then return end
-        local dataTuple = box.tuple.new(data[1]):update({{'!', art.core.bucketFieldNumber(space), bucket_id}})
-        return {dataTuple, data[2]}
+        if not (data) or not (bucket_id) then
+            return
+        end
+        local dataTuple = box.tuple.new(data[1]):update({ { '!', art.core.bucketFieldNumber(space), bucket_id } })
+        return { dataTuple, data[2] }
     end,
 
     removeBucket = function(space, data)
-        if not(data) or not(data[2]) then return data end
-        local dataTuple = box.tuple.new(data[1]):update({{'#', art.core.bucketFieldNumber(space), 1}})
-        return {dataTuple, data[2]}
+        if not (data) or not (data[2]) then
+            return data
+        end
+        local dataTuple = box.tuple.new(data[1]):update({ { '#', art.core.bucketFieldNumber(space), 1 } })
+        return { dataTuple, data[2] }
     end,
 
     mapBucket = function(space, key)
         local mapping_entry = box.space[space]:get(key)
-        if not (mapping_entry) then return end
+        if not (mapping_entry) then
+            return
+        end
         return mapping_entry.bucket_id
     end,
 
     correctUpdateOperations = function(space, operations)
         for index, command in pairs(operations[1]) do
-            if command[2] >= art.core.bucketFieldNumber(space) then operations[1][index][2] = command[2] + 1 end
+            if command[2] >= art.core.bucketFieldNumber(space) then
+                operations[1][index][2] = command[2] + 1
+            end
         end
         return operations
     end,
@@ -117,7 +135,7 @@ local core = {
             end,
 
             notEquals = function(object, fieldno, value)
-                return not(object[fieldno] == value)
+                return not (object[fieldno] == value)
             end,
 
             more = function(object, fieldno, value)
@@ -133,7 +151,7 @@ local core = {
             end,
 
             notInRange = function(object, fieldno, startValue, endValue)
-                return not((object[fieldno] >= startValue) and (object[fieldno] <= endValue))
+                return not ((object[fieldno] >= startValue) and (object[fieldno] <= endValue))
             end,
 
             like = function(object, fieldno, pattern)
