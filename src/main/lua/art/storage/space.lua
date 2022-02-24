e3351a5c4955a74c7c04613e03df3bd215daf46c
anton.bashirov <anton.sh.local@gmail.com>
2022-02-24 05:36:51 +0300

    ART

--- src/main/lua/art/storage/space.lua
+++ src/main/lua/art/storage/space.lua
@@ -1,3 +1,5 @@
+stream = require("art.storage.stream")
+
 local space = {
     findFirst = function(space, key)
         return box.space[space]:get(key)
@@ -7,22 +9,24 @@ local space = {
         local result = {}
         for _, key in pairs(keys) do
             local value = box.space[space]:get(key)
-            if value ~= nil then table.insert(result, value) end
+            if value ~= nil then
+                table.insert(result, value)
+            end
         end
         return result
     end,
 
-    find = function(space, key, operators)
-        local generator, param, state = box.space[space]:pairs(key)
+    find = function(space, operators)
+        local generator, param, state = box.space[space]:pairs()
 
         for _, operator in pairs(operators) do
             local name = operator[1]
             local parameters = operator[2]
-            local functor = art.storage.stream.select(name)
+            local functor = stream.select(name)
             generator, param, state = functor(generator, param, state, parameters)
         end
 
-        return art.storage.stream.collect(generator, param, state)
+        return stream.collect(generator, param, state)
     end,
 
     count = function(space)
