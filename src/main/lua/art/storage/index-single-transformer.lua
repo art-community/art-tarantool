00c4d718a5fede338204b69071be01660833a812
anton.bashirov <anton.sh.local@gmail.com>
2022-03-13 04:08:20 +0300

    ART

--- src/main/lua/art/storage/index-single-transformer.lua
+++ src/main/lua/art/storage/index-single-transformer.lua
@@ -1,4 +1,8 @@
 local transformer = {
+    delete = function(space, index, key)
+        return box.space[space].index[index]:delete(key)
+    end,
+
     update = function(space, index, key, commands)
         return box.space[space].index[index].update(space, key, commands)
     end,
