ed817a8ac7c3b57677d70dab967fd48fe80762a0
anton.bashirov <anton.sh.local@gmail.com>
2022-03-13 20:17:13 +0300

    ART

--- src/main/lua/art/storage/index-multiple-transformer.lua
+++ src/main/lua/art/storage/index-multiple-transformer.lua
@@ -2,8 +2,15 @@ local transformer = {
     delete = function(space, index, keys)
         return box.atomic(function()
             local results = {}
+            local foundIndex = box.space[space].index[index]
             for _, key in pairs(keys) do
-                table.insert(results, box.space[space].index[index]:delete(key))
+                if foundIndex.unique and #key == 1 then
+                    table.insert(results, box.space[space].index[index]:delete(key))
+                else
+                    for _, element in foundIndex:pairs(key) do
+                        table.insert(results, box.space[space].index[index]:delete(element))
+                    end
+                end
             end
             return results
         end)
--- src/main/lua/art/storage/index-single-transformer.lua
+++ src/main/lua/art/storage/index-single-transformer.lua
@@ -1,10 +1,24 @@
 local transformer = {
     delete = function(space, index, key)
-        return box.space[space].index[index]:delete(key)
+        local foundIndex = box.space[space].index[index]
+        if foundIndex.unique and #key == 1 then
+            return box.space[space].index[index]:delete(key)
+        else
+            for _, element in foundIndex:pairs(key) do
+                return box.space[space].index[index]:delete(element)
+            end
+        end
     end,
 
     update = function(space, index, key, commands)
-        return box.space[space].index[index]:update(key, commands)
+        local foundIndex = box.space[space].index[index]
+        if foundIndex.unique and #key == 1 then
+            return box.space[space].index[index]:update(key, commands)
+        else
+            for _, element in foundIndex:pairs(key) do
+                return box.space[space].index[index]:update(element, commands)
+            end
+        end
     end,
 }
 
